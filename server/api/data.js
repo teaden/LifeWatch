e812c6203cbeca9a953c00782c898e6d67dfc210
Tyler Eaden <teaden@Tylers-MacBook-Pro.local>
2022-08-02 04:49:18 -0400

    Feature: Built express route for handling data from user

--- server/api/data.js
+++ server/api/data.js
@@ -1,2 +1,11 @@
 const router = require('express').Router();
 module.exports = router;
+
+router.post('/', async (req, res, next) => {
+    try {
+        // Do regression predictions here and send back
+        res.status(201).json();
+    } catch (error) {
+        next(error);
+    }
+});
