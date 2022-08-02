e816406311cec8004c250b2084876cd5ff9288e6
Tyler Eaden <teaden@Tylers-MacBook-Pro.local>
2022-08-02 04:45:58 -0400

    Added redux boilerplate code

---
+++ client/store/data.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const RECEIVE_DATA = 'RECEIVE_DATA';
+
+const defaultState = {};
+
+const receiveData = data => ({ type: RECEIVE_DATA, data });
+
+export const attemptReceiveData = data => {
+    return async dispatch => {
+        try {
+            const res = await axios.post('/api/data', data);
+            dispatch(receiveData(res.data.data));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+};
+
+export default function (state = defaultState, action) {
+    switch (action.type) {
+        case RECEIVE_DATA:
+            return action.data;
+        default:
+            return state;
+    }
+}
