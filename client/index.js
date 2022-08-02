a2c0d0c1b2e94016b0887942e48852dec26e95ff
Tyler Eaden <teaden@Tylers-MacBook-Pro.local>
2022-08-02 04:03:29 -0400

    Feature: Added all necessary dependencies and modified base React code

--- client/index.js
+++ client/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
 );
--- server/index.js
+++ server/index.js
@@ -1,33 +1,27 @@
-const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
-const PORT = process.env.PORT || 9000;
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const routes = require('./api/index');
+
+const port = 9000;
 const app = express();
-module.exports = app;
 
-const buildApp = () => {
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(bodyParser.json());
 
-    app.use(morgan('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(compression());
+app.use('/', routes);
 
-    app.use('/api', require('./api'));
-
-    app.use(express.static(path.join(__dirname, '..', 'public')));
-
-    app.use((req, res, next) => {
-        if (path.extname(req.path).length) {
-            const err = new Error('Not found');
-            err.status = 404;
-            next(err);
-        } else {
-            next();
-        }
-    });
-};
+app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+        const err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    } else {
+        next();
+    }
+});
 
 app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'));
@@ -36,16 +30,17 @@ app.use('*', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err);
     console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Internal Server Error');
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
 
-const listen = () => {
-    const server = app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+const startListening = () => {
+    const server = app.listen(port, () =>
+        console.log(`Listening on ${port}`)
+    );
 };
 
-async function launchApp() {
-    await buildApp();
-    await listen();
+async function bootApp() {
+    await startListening();
 }
 
-launchApp();
+bootApp();
